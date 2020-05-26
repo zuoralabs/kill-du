60f7185af64253c876934152db8407ded8aa056c
Andrew Lee <andrew.lee@zuora.com>
2020-05-26 10:21:22 -0700

    Create a config file in /etc

--- install.zsh
+++ install.zsh
@@ -1,9 +1,16 @@
 #!/usr/bin/env zsh
 
+prog_name=killer-daemon
+config_file=${prog_name}_config
+
 mkdir -v -p '/opt/local/bin'
 cp -iv killer-daemon.zsh '/opt/local/bin'
 
 cp -iv init-killer-daemon.plist /Library/LaunchDaemons
 
-echo 'loading /Library/LaunchDaemons/init-killer-daemon.plist'
-launchctl load -w /Library/LaunchDaemons/init-killer-daemon.plist
+if [[ ! -e /etc/${prog_name} ]]; then
+    echo "# list of process names to kill" > /etc/${config_file}
+fi
+
+echo "loading /Library/LaunchDaemons/init-${prog_name}.plist"
+launchctl load -w /Library/LaunchDaemons/init-${prog_name}.plist
